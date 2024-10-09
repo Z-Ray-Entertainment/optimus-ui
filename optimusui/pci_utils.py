852eb05edadf7bbf5a3c89b2532beef86d631ae5
Vortex Acherontic <vortex@z-ray.de>
2024-10-09 13:45:26 +0200

    has_nv_gpu improved

--- optimusui/pci_utils.py
+++ optimusui/pci_utils.py
@@ -6,12 +6,8 @@ from optimusui.const import PCI_DEVICE_PATH, DEVICE_CLASS_GPU, NVIDIA_VENDOR_ID
 
 
 def has_nvidia_gpu():
-    nvidia_gpus = find_nvidia_gpu()
-    if len(nvidia_gpus) > 0:
-        for gpu in nvidia_gpus:
-            device_name = gpu.resolve_device_name()
-        return True
-    return False
+    return len(find_nvidia_gpu()) > 0
+
 
 def find_nvidia_gpu():
     nvidia_gpus = []
@@ -22,6 +18,7 @@ def find_nvidia_gpu():
                 nvidia_gpus.append(device_properties)
     return nvidia_gpus
 
+
 def find_all_gpus():
     all_gpus = []
     for subdir, dirs, files in os.walk(PCI_DEVICE_PATH):
@@ -31,9 +28,11 @@ def find_all_gpus():
                 all_gpus.append(device_properties)
     return all_gpus
 
+
 def _get_device_info(device):
     device_uevent_result = subprocess.run(["cat", PCI_DEVICE_PATH + device + "/uevent"], stdout=subprocess.PIPE)
     return device_uevent_result.stdout.decode("utf-8").rstrip().split("\n")
+
 
 def _build_device_properties(device_info):
     pci_class: str
@@ -53,6 +52,7 @@ def _build_device_properties(device_info):
                 pci_slot = prop[1]
     return DeviceProperties(pci_class, device_id, vendor_id, pci_slot)
 
+
 class DeviceProperties:
     def __init__(self, pci_class, device_id, vendor_id, pci_slot):
         self.pci_class = pci_class
@@ -70,7 +70,7 @@ class DeviceProperties:
         pci_look_up = self.vendor_id + ":" + self.device_id
         device_lspci_result = subprocess.run(["lspci", "-d", pci_look_up], stdout=subprocess.PIPE)
         lspci = device_lspci_result.stdout.decode("utf-8").rstrip().split(":")
-        return lspci[len(lspci)-1].strip()
+        return lspci[len(lspci) - 1].strip()
 
     def resolve_device_architecture(self):
         pass
