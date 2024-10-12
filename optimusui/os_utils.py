d4cae872b72d45735e5c86767c0f9811e3c5b733
Vortex Acherontic <vortex@z-ray.de>
2024-10-12 10:56:24 +0200

    Update os_utisl docs

--- optimusui/os_utils.py
+++ optimusui/os_utils.py
@@ -7,26 +7,28 @@ Various OS and flatpak related utilities
 
 FLATPAK_SPAWN = ["flatpak-spawn", "--host"]
 
-'''
-Test if the app is running as  flatpak
-'''
 def is_flatpak() -> bool:
+    """
+    Test if the app is running as  flatpak
+    """
     return environ.get("FLATPAK_ID") is not None
 
-'''
-Runs a command as a subprocess but does not return the result and does not
-wait for the command to finish. Useful for running binaries which do not terminate
-'''
+
 def run_command_no_pipe(base_command: []):
+    """
+    Runs a command as a subprocess but does not return the result and does not
+    wait for the command to finish. Useful for running binaries which do not terminate
+    """
     if is_flatpak():
         return subprocess.run(FLATPAK_SPAWN + base_command)
     return subprocess.run(base_command)
 
-'''
-Runs a given array as a subprocess and returns the result.
-If the app is running inside flatpak it will use flatpak-spawn
-'''
+
 def run_command(base_command: []):
+    """
+    Runs a given array as a subprocess and returns the result.
+    If the app is running inside flatpak it will use flatpak-spawn
+    """
     if is_flatpak():
         return subprocess.run(FLATPAK_SPAWN + base_command, stdout=subprocess.PIPE)
     return subprocess.run(base_command, stdout=subprocess.PIPE)
