1867e788accf6b9677c673ec0d1506561a284d25
Vortex Acherontic <vortex@z-ray.de>
2024-10-15 19:35:25 +0200

    Fix bbswtich power state cmd

--- optimusui/bbswitch.py
+++ optimusui/bbswitch.py
@@ -14,11 +14,11 @@ class PowerState(Enum):
 def force_power_state(power_on: bool):
     power_cmd = ["echo"]
     if power_on:
-        power_cmd += [PowerState.ON]
+        power_cmd += [PowerState.ON.value]
     else:
-        power_cmd += [PowerState.OFF]
+        power_cmd += [PowerState.OFF.value]
     power_cmd += [">", "/proc/acpi/bbswitch"]
-
+    os_utils.run_command_as_root(power_cmd)
 
 def has_bbswitch():
     loaded_mods = ["cat", "/proc/modules"]
